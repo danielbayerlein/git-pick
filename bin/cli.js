1c713fe48975829e818ed2a55434a6e0effdf644
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-11-12 15:59:26 +0100

    Add functionality

---
+++ bin/cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const program = require('commander');
+const gitPick = require('../lib');
+const pkg = require('../package.json');
+
+program
+  .version(pkg.version)
+  .arguments('<commit> <branches...>')
+  .action((commit, branches) => {
+    // Execute git-pick with the given commmit and branches
+    gitPick(commit, branches);
+  })
+  .parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
