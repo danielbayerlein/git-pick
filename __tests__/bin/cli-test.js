370e4d8f9583adfa4f37fd1af0ca40af0dfc03bc
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-11-14 11:03:17 +0100

    Add bin/cli.js test

---
+++ __tests__/bin/cli-test.js
@@ -0,0 +1,31 @@
+jest.mock('../../lib');
+jest.mock('../../package.json', () => ({
+  version: '1.0.0',
+}));
+
+const program = require('commander');
+const gitPick = require('../../lib');
+
+describe('cli.js', () => {
+  beforeEach(() => {
+    program.version = jest.fn().mockReturnThis();
+    program.arguments = jest.fn().mockReturnThis();
+    program.action = jest.fn().mockImplementation(function action(cb) {
+      cb('1234abc', ['branch1', 'branch2']);
+      return this;
+    });
+    program.parse = jest.fn();
+    program.args = jest.fn();
+    program.help = jest.fn();
+
+    require('../../bin/cli'); // eslint-disable-line global-require
+  });
+
+  it('to be called program.version with "1.0.0"', () => {
+    expect(program.version).toBeCalledWith('1.0.0');
+  });
+
+  it('to be called gitPick with "\'1234abc\', [\'branch1\', \'branch2\']"', () => {
+    expect(gitPick).toBeCalledWith('1234abc', ['branch1', 'branch2']);
+  });
+});
